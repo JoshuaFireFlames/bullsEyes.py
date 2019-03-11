f4c3f9601be00d2f60d1274f88062877230118a9
csPython <cspython@lpf0lwntb.mbhs.lab>
2019-03-11 15:58:49 -0400

    Window Created.

---
+++ bullsEyeJW.py
@@ -0,0 +1,12 @@
+#bullsEye by joshGara
+
+from graphics import *
+
+ciSz = 50
+
+
+bullWin = GraphWin("BullsEye", ciSz*10, ciSz*10)
+bullWin.setCoords(0, 0, ciSz*10, ciSz*10)
+
+bullWin.getMouse()
+bullWin.close()
