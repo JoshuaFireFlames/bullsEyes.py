b6bff9ed579c9825f82331782edec633052f7c7c
csPython <cspython@lpf0lwntb.mbhs.lab>
2019-03-13 09:42:59 -0400

    Created circle in window.

--- bullsEyeJW.py
+++ bullsEyeJW.py
@@ -8,5 +8,9 @@ ciSz = 50
 bullWin = GraphWin("BullsEye", ciSz*10, ciSz*10)
 bullWin.setCoords(0, 0, ciSz*10, ciSz*10)
 
+Circle  = Circle(Point(250, 250), 30)
+Circle.setFill(color_rgb(34,139,34))
+Circle.draw(bullWin)
+
 bullWin.getMouse()
 bullWin.close()
