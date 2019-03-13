e0a673117e2e9a55a3b16bc4ac04c2f0b5dd35fe
csPython <cspython@lpf0lwntb.mbhs.lab>
2019-03-13 09:52:27 -0400

    Created a custom function for the circle in the window.

--- bullsEyeJW.py
+++ bullsEyeJW.py
@@ -2,15 +2,20 @@
 
 from graphics import *
 
+def draw_ci(cX, cY, size, color, bullWin):
+    circle = Circle(Point(cX, cY),Point(cX, cY))
+    circle.setFill(color)
+    circle.draw(bullWin)
+
 ciSz = 50
 
 
 bullWin = GraphWin("BullsEye", ciSz*10, ciSz*10)
 bullWin.setCoords(0, 0, ciSz*10, ciSz*10)
 
-Circle  = Circle(Point(250, 250), 30)
-Circle.setFill(color_rgb(34,139,34))
-Circle.draw(bullWin)
+circle  = Circle(Point(250, 250), 30)
+circle.setFill(color_rgb(34,139,34))
+circle.draw(bullWin)
 
 bullWin.getMouse()
 bullWin.close()
