be9f20f4945888e78207d36506578d7fcef9b1eb
csPython <cspython@lpf0lwntb.mbhs.lab>
2019-03-13 15:41:38 -0400

    Fixed circle function.

--- bullsEyeJW.py
+++ bullsEyeJW.py
@@ -3,7 +3,7 @@
 from graphics import *
 
 def draw_ci(cX, cY, size, color, bullWin):
-    circle = Circle(Point(cX, cY),Point(cX, cY))
+    circle = Circle(Point(cX, cY), size)
     circle.setFill(color)
     circle.draw(bullWin)
 
@@ -13,9 +13,12 @@ ciSz = 50
 bullWin = GraphWin("BullsEye", ciSz*10, ciSz*10)
 bullWin.setCoords(0, 0, ciSz*10, ciSz*10)
 
-circle  = Circle(Point(250, 250), 30)
-circle.setFill(color_rgb(34,139,34))
-circle.draw(bullWin)
+##circle  = Circle(Point(250, 250), 30)
+##circle.setFill(color_rgb(34,139,34))
+##circle.draw(bullWin)
+
+draw_ci(250, 250, ciSz, "dark blue", bullWin)
+
 
 bullWin.getMouse()
 bullWin.close()
