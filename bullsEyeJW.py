f4cb575e73d686cd4098355fe5d143b471d0a166
csPython <cspython@lpf0lwntb.mbhs.lab>
2019-03-13 15:59:58 -0400

    Used draw_cir function to create one bullsEye.

--- bullsEyeJW.py
+++ bullsEyeJW.py
@@ -2,10 +2,10 @@
 
 from graphics import *
 
-def draw_ci(cX, cY, size, color, bullWin):
+def draw_ci(cX, cY, size, color, win):
     circle = Circle(Point(cX, cY), size)
     circle.setFill(color)
-    circle.draw(bullWin)
+    circle.draw(win)
 
 ciSz = 50
 
@@ -13,11 +13,14 @@ ciSz = 50
 bullWin = GraphWin("BullsEye", ciSz*10, ciSz*10)
 bullWin.setCoords(0, 0, ciSz*10, ciSz*10)
 
-##circle  = Circle(Point(250, 250), 30)
-##circle.setFill(color_rgb(34,139,34))
-##circle.draw(bullWin)
 
 draw_ci(250, 250, ciSz, "dark blue", bullWin)
+draw_ci(250, 250, ciSz - 10, "white", bullWin)
+draw_ci(250, 250, ciSz - 20, "dark blue", bullWin)
+draw_ci(250, 250, ciSz - 30, "white", bullWin)
+draw_ci(250, 250, ciSz - 40, "dark blue", bullWin)
+
+
 
 
 bullWin.getMouse()
