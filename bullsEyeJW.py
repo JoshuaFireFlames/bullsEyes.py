0453a6328f73f1940d85e35b826e5ff58088c942
csPython <cspython@lpf0lwntb.mbhs.lab>
2019-03-14 17:33:51 -0400

    A custom bull's eye function calls the custom circle function to create a bull's eye in the window.

--- bullsEyeJW.py
+++ bullsEyeJW.py
@@ -7,20 +7,30 @@ def draw_ci(cX, cY, size, color, win):
     circle.setFill(color)
     circle.draw(win)
 
+def draw_be(bX, bY, bSize, bColor1, bColor2, bWin):
+
+##    for i in range (5):
+##        if (i) % 2 == 1:
+##            cCol = bColor1
+##            else:
+##                
+##                 draw_ci(bX, bY, bSize, cCol, bWin)
+##                 bSize -= 10
+        draw_ci(bX, bY, bSize, bColor1, bWin)
+        draw_ci(bX, bY, bSize - 10, bColor2, bWin)
+        draw_ci(bX, bY, bSize - 20, bColor1, bWin)
+        draw_ci(bX, bY, bSize - 30, bColor2, bWin)
+        draw_ci(bX, bY, bSize - 40, bColor1, bWin)
+
+
+
 ciSz = 50
 
 
 bullWin = GraphWin("BullsEye", ciSz*10, ciSz*10)
 bullWin.setCoords(0, 0, ciSz*10, ciSz*10)
 
-
-draw_ci(250, 250, ciSz, "dark blue", bullWin)
-draw_ci(250, 250, ciSz - 10, "white", bullWin)
-draw_ci(250, 250, ciSz - 20, "dark blue", bullWin)
-draw_ci(250, 250, ciSz - 30, "white", bullWin)
-draw_ci(250, 250, ciSz - 40, "dark blue", bullWin)
-
-
+draw_be(ciSz*5, ciSz*5, ciSz, "dark blue", "white", bullWin)
 
 
 bullWin.getMouse()
